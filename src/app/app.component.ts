f5184481fbfb327a59e680890dae76d0d3e2aa28
SEAN SKELTON <G00361169@gmit.ie>
2020-04-24 20:42:28 +0100

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,22 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/angular/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/angular/css/normalize.css';
+import '@ionic/angular/css/structure.css';
+import '@ionic/angular/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import "@ionic/angular/css/padding.css";
+import "@ionic/angular/css/float-elements.css";
+import "@ionic/angular/css/text-alignment.css";
+import "@ionic/angular/css/text-transformation.css";
+import "@ionic/angular/css/flex-utils.css";
+
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
